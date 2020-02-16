c631c539e93d560cd45316dc1ca644b5fdfb17f0
Gabriel S <desouza.gabriel@icloud.com>
2020-02-16 14:54:33 -0300

    build (app) Adiciona Vue Router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import router from './router'
+import firebase from 'firebase'
 import vuetify from './plugins/vuetify'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
+const config = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+}
+
 Vue.config.productionTip = false
+firebase.initializeApp(config)
 
 new Vue({
+  router,
   store,
   vuetify,
   render: h => h(App)
